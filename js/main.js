a3dc98ac09ae609d73dfdbf90da47bab56a876fd
William White, Jr <68568420+wewjr82@users.noreply.github.com>
2022-05-11 15:11:55 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let init = () => {
+  let container = document.getElementsByClassName("jumbo-slider__container")[0],
+    slides = document.getElementsByClassName("jumbo-slider__slide"),
+    circles = document.getElementsByClassName("jumbo-slider__circle"),
+    links = document.getElementsByClassName("jumbo-slider__link"),
+    current = 1,
+    time = 6000;
+
+  //add animation class to slide
+  slides[0].classList.add("jumbo-slider__slide-active");
+  links[current - 1].classList.add("jumbo-slider__link--active");
+
+  //update elipsis and links
+  let updateNav = () => {
+    console.log(`Update current: ${current}`);
+    for (let i = 0; i < slides.length; i++) {
+      links[i].classList.remove("jumbo-slider__link--active");
+      circles[i].classList.remove("jumbo-slider__circle--filled");
+    }
+    links[current - 1].classList.add("jumbo-slider__link--active");
+    circles[current - 1].classList.add("jumbo-slider__circle--filled");
+  };
+  let startSliding = () => {
+    setInterval(() => {
+      console.log(`current: ${current}`);
+      slides[1].classList.add("jumbo-slider__slide--active");
+      slides[0].classList.remove("jumbo-slider__slide--active");
+
+      if (current < slides.length) {
+        current++;
+        updateNav(current);
+      } else {
+        current = 1;
+      }
+      container.appendChild(slides[0].cloneNode([true]));
+      container.removeChild(slides[0]);
+    }, 6000);
+  };
+  startSliding();
+};
+
+init();
